4a1194b52febb8083c3754f1f3236f9cff03c66d
DESKTOP-O1K409M\user <definejae234@gmail.com>
2025-07-10 18:17:40 +0900

    * //////////////////////////////////////////////////////////////////////////////////////////////////// * 날짜 : 2025.07.10 * 버전 : Ver_25_07_10_001 * 내용 : Java Algorithm github 연동 작업 진행 * ////////////////////////////////////////////////////////////////////////////////////////////////////

---
+++ Algorithm_main/src/Main.java
@@ -0,0 +1,24 @@
+
+/*
+* ////////////////////////////////////////////////////////////////////////////////////////////////////
+* 날짜 : 2025.07.10
+* 버전 : Ver_25_07_10_001
+* 내용 : Java Algorithm github 연동 작업 진행
+* ////////////////////////////////////////////////////////////////////////////////////////////////////
+* */
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
